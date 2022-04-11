bce51129dc234716e63f8bea929e300a05ec496c
Anastasia <achernova1551@gmail.com>
2022-04-12 00:36:59 +0300

    comit

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,9 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-
-
-
-/*const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+const buttonPurchase = document.querySelector('.countries-description__button');
+const modalTrip = document.querySelector('.modal-trip');
+const modalTripclose = document.querySelector('.modal-trip__button');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -31,6 +17,28 @@ navToggle.addEventListener('click', function() {
   }
 });
 
+
+for (let i = 0; i < buttonPurchase.length; i++) {
+    buttonPurchase[i].addEventListener('click', function(evt) {
+        evt.preventDefault();
+        modalTrip.classList.add('modal-trip--show');
+    });
+}
+modalTripclose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalTrip.classList.remove('modal-trip--show');
+});
+
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (modalTrip.classList.contains('modal-trip--show')) {
+            evt.preventDefault();
+            modalTrip.classList.remove('modal-trip--show');
+        }
+    }
+});
+
+/*
 const link = document.querySelector('.js-login');
 const popup = document.querySelector('.modal');
 const close = popup.querySelector('.modal__button--close');
