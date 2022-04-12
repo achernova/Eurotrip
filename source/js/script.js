37e2f613285010094db4d35dd70110168df26103
Anastasia <achernova1551@gmail.com>
2022-04-12 12:32:31 +0300

    commit

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,14 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 const buttonPurchase = document.querySelector('.countries-description__button');
-const modalTrip = document.querySelector('.modal-trip');
-const modalTripclose = document.querySelector('.modal-trip__button');
+const modalTrip = document.querySelector('#modal-show');
+const modalTripclose = document.querySelector('.modal-trip__close');
+const priceButton = document.querySelectorAll('.price__button');
+
+const tabList = document.querySelectorAll('.description__tabs-link');
+
+const tabCountry = document.querySelectorAll('.countries-description__item');
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -17,83 +23,39 @@ navToggle.addEventListener('click', function() {
   }
 });
 
+buttonPurchase.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalTrip.classList.add('modal-trip--show');
+});
 
-for (let i = 0; i < buttonPurchase.length; i++) {
-    buttonPurchase[i].addEventListener('click', function(evt) {
-        evt.preventDefault();
-        modalTrip.classList.add('modal-trip--show');
-    });
+modalTripclose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalTrip.classList.remove('modal-trip--show');
+});
+
+for (let i = 0; i < priceButton.length; i++) {
+  priceButton[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      modalTrip.classList.add('modal-trip--show');
+  });
 }
-modalTripclose.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalTrip.classList.remove('modal-trip--show');
-});
-
-window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27) {
-        if (modalTrip.classList.contains('modal-trip--show')) {
-            evt.preventDefault();
-            modalTrip.classList.remove('modal-trip--show');
-        }
-    }
-});
 
 /*
-const link = document.querySelector('.js-login');
-const popup = document.querySelector('.modal');
-const close = popup.querySelector('.modal__button--close');
-const form = popup.querySelector('form');
-const login = popup.querySelector('[name=login]');
-const password = popup.querySelector('[name=password]');
-
-let isStorageSupport = true;
-let storage = '';
-
-try {
-  storage = localStorage.getItem('login');
-} catch (err) {
-  isStorageSupport = false;
-}
-
-link.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.add('modal--show');
-
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
-});
-
-close.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.remove('modal--show');
-  popup.classList.remove('modal--error');
-});
-
-form.addEventListener('submit', function (evt) {
-  if (!login.value || !password.value) {
+console.log(tabList.length)
+for (let i = 0; i < tabList.length; i++) {
+  tabList[i].addEventListener('click', function(evt) {
     evt.preventDefault();
-    popup.classList.remove('modal--error');
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add('modal--error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('login', login.value);
+    const tabId = evt.target.getAttribute('data-id');
+    for (let i = 0; i < tabCountry.length; i++) {
+      tabList[i].classList.remove('countries-description__item--active');
     }
-  }
-});
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-
-    if (popup.classList.contains('modal--show')) {
-      popup.classList.remove('modal--show');
-      popup.classList.remove('modal--error');
+    for (let i = 0; i < tabList.length; i++) {
+      tabList[i].classList.remove('description__tabs-link--active');
     }
-  }
-});
-*/
+    console.log(tabId)
+    console.log(document.getElementById(tabId))
+    document.getElementById(tabId).classList.add('countries-description__item--active')
+    evt.target.classList.add('description__tabs-link--active')
+
+  });
+}*/
